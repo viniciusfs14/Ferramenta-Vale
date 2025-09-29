3003619c4ec5363ceeaceeb5b90521badb97213e
Vinícius Fernandes <135647686+viniciusfs14@users.noreply.github.com>
2025-09-29 08:52:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ui.resources_rc import *
 import webbrowser          
 from PySide6.QtGui import QIcon
 from PySide6.QtCore import QSize
+import subprocess
 
 class MainWindow(QWidget, Ui_Tela):
     def __init__(self):
@@ -17,11 +18,16 @@ class MainWindow(QWidget, Ui_Tela):
         self.pushButton_2.setIcon(QIcon(":/newPrefix/radix_logo.jpg"))
         self.pushButton_2.setIconSize(QSize(150,150))
         self.pushButton.setIconSize(QSize(150,150))
+        self.pushButton_2.clicked.connect(self.open_planilha)
 
     def open_pydaq_website(self):
-        url = "https://samirmartins.github.io/pydaq/"
+        url = "https://vale.service-now.com/now/nav/ui/classic/params/target/%24vtb.do%3Fsysparm_board%3D8c50d5d4fbf342505019f3636eefdc7a"
         webbrowser.open(url)
         
+    def open_planilha(self):
+        controle = r"Controle Suporte VALE - Catalogadas - Oficial.xlsm"
+        subprocess.Popen(["start", "excel", controle], shell=True)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
